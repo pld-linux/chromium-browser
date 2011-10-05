e6c6c1d9af0b7bacb169a19bee27a05057d34fea
Arkadiusz Miśkiewicz <arekm@maven.pl>
2011-10-05 12:20:47 +0000

    - unfinished
    Changed files:
        get-source.sh -> 1.1

---
+++ get-source.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+# based on debian/rules for chromium package
+
+set -e
+set -x
+
+CHANNEL="beta"
+# See Staying Green More Of The Time at http://dev.chromium.org/developers/how-tos/get-the-code
+USE_GREEN_REV=1
+
+GCLIENT_URL="http://src.chromium.org/svn/trunk/tools/depot_tools"
+CHROMIUM_URL="http://src.chromium.org/svn/trunk/src"
+CHROMIUM_RLZ="http://src.chromium.org/svn/releases"
+DEPS_URL="http://src.chromium.org/svn/trunk/deps/third_party"
+GREEN_REV_URL="http://chromium-status.appspot.com/lkgr"
+CHANNELS_URL="http://omahaproxy.appspot.com/"
+
+CHANNEL="beta"
+
+TMP_DIR=$(pwd)/chromium-browser
+
+VERSION=$(wget -qO - "$CHANNELS_URL" | grep -i "^linux,${CHANNEL}" | cut -d, -f3)
+
+
+rm -rf $TMP_DIR
+install -d $TMP_DIR
+svn co "$GCLIENT_URL" chromium-browser/tools/depot_tools
+cd $TMP_DIR
+if [ "$USE_GREEN_REV" -eq 1 ]; then
+	REVISION=$(wget -qO - "${GREEN_REV_URL}")
+	./tools/depot_tools/gclient config "${CHROMIUM_URL}" "${GREEN_REV_URL}"
+else
+	REVISION=101024
+	./tools/depot_tools/gclient config "${CHROMIUM_URL}"
+fi
+
+REVISION="--revision src@${REVISION}"
+
+cd $TMP_DIR
+./tools/depot_tools/gclient update --nohooks ${REVISION}
+
+cd $TMP_DIR
+SDIR=`grep '"name"' .gclient | cut -d\" -f4`
+perl -i~ -pe 's%(.python., .src/build/gyp_chromium.)%"echo", "#disabled#", $1%' $SDIR/DEPS
+./tools/depot_tools/gclient runhooks
+mv $SDIR/DEPS~ $SDIR/DEPS
+
+cd $(TMP_DIR)/src && find . -type f \( -iname \*.exe -o -iname \*.dll -o -iname \*.pdb -o -name \*.o -o -name \*.a -o -name \*.dylib \) -exec rm -fv {} \; > REMOVED-bin_only.txt
+wc -l $(TMP_DIR)/src/REMOVED-*.txt
+
+
