5aafdaeeb26cf01070ac1646ab2717107d67fcae
Elan Ruusamäe <glen@pld-linux.org>
2011-10-13 20:55:26 +0000

    - create logfile of whole update process. upload as well
    Changed files:
        get-source.sh -> 1.2.2.2

--- get-source.sh
+++ get-source.sh
@@ -5,10 +5,11 @@ CHANNEL=beta
 PACKAGE_NAME=chromium-browser
 WORK_DIR=$HOME/bzr/$PACKAGE_NAME.head.daily
 CHROMIUM=$HOME/svn/$PACKAGE_NAME-$CHANNEL
+LOGFILE=$(mktemp $WORK_DIR/$PACKAGE_NAME-$CHANNEL.XXXXXX)
 
 cd "$WORK_DIR"
 dpkg-architecture -c \
-./debian/rules get-orig-source LOCAL_BRANCH=$CHROMIUM CHANNEL=$CHANNEL
+./debian/rules get-orig-source LOCAL_BRANCH=$CHROMIUM CHANNEL=$CHANNEL > $LOGFILE 2>&1
 
 tarball=$(ls $PACKAGE_NAME*.orig.tar.gz)
 count=$(echo "$tarball" | wc -w)
@@ -18,9 +19,12 @@ if [ "$count" != 1 ]; then
 	exit 1
 fi
 
-ls -lh $tarball
-chmod 644 $tarball
-scp -pr $tarball carme.pld-linux.org:public_html/chromium-browser/src/$CHANNEL/
+logfile=$(basename $tarball .orig.tar.gz).log
+mv $LOGFILE $logfile
+
+ls -lh $tarball $logfile
+chmod 644 $tarball $logfile
+scp -pr $logfile $tarball carme.pld-linux.org:public_html/chromium-browser/src/$CHANNEL/
 
 install -d archive/$CHANNEL
-mv $tarball archive/$CHANNEL
+mv $logfile $tarball archive/$CHANNEL
